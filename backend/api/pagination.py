4fc606b9cc04d064e682f2502f03ab41a7fb2444
YURESSA <goshrenko@gmail.com>
2025-05-09 10:08:54 +0500

    remove old version

--- backend/api/pagination.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import pagination
-
-from foodgram import constants
-
-
-class Standard_Pagination(pagination.PageNumberPagination):
-    page_size = constants.PAGE_SIZE
-    page_size_query_param = 'limit'
--- backend/auth_user/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class Auth_UserConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'auth_user'
-    verbose_name = 'Пользователи'
